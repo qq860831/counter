6b2cd2a2f710ca2ef4f56a7c87919ea3fa81adcb
qq860831 <qq860831@gmail.com>
2020-07-03 18:17:02 +0800

    ok

--- counter.js
+++ counter.js
@@ -1,12 +1,11 @@
 // 當頁面 準備完成時
 $(document).ready(function () {
     // 當視窗 捲動時
-    $(selector).scroll(function () {
+    $(window).scroll(function () {
         // var 變數=資料
-        var offset = $("#test").offset();
-        //取得元素位移資料  
-        var top = offset.top;
-        var left = offset.left;
+        var offset = $("#test").offset();  //取得元素位移資料  
+        var top = offset.top;    //上方
+        var left = offset.left;  //左方
 
         // console.log("元素上方:"+top);
 
@@ -18,9 +17,17 @@ $(document).ready(function () {
         // 如果...發生，就...
         // if(條件)(程式)
         if (windowTop > top) {
-            console.log("開始執行動畫...");
+            // console.log("開始執行動畫...");
+
+            // 選取動畫({參數}，{選項})
+            $("#test").animate({
+                num : 777            //數字
+            }, {
+                duration:2000,     //時間
+                step:(now)=> {      //步驟
+                    $("#test").text(Math.floor(now));     //文字(步驟)Math.floor去小數點
+                }
+            })
         }
     });
-
-
 });
