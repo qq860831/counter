70e89853a82182642c6a4bb25aec11e0e3d6877e
HWChia <66512878+xmas21@users.noreply.github.com>
2020-07-05 11:07:18 +0800

    123

--- counter.js
+++ counter.js
@@ -2,41 +2,45 @@
 $(document).ready(function () {
     // 當視窗 捲動時
     $(window).scroll(function () {
-        // var 變數=資料
-        var offset = $("#test").offset();  //取得元素位移資料  
-        var top = offset.top;    //上方
-        var left = offset.left;  //左方
 
-        // console.log("元素上方:"+top);
+        $("*").each(function (index, element) {
+            // element == this  當下搜尋到的元素
+            // var 變數=資料
+            var offset = $(this).offset();  //取得元素位移資料  
+            var top = offset.top;    //上方
+            var left = offset.left;  //左方
 
-        var windowTop = $(window).scrollTop();
+            // console.log("元素上方:"+top);
 
-        // console.log("視窗的上方:"+windowTop);
+            var windowTop = $(window).scrollTop();
 
-        // 取得參數:時間、數字與位移
-        var number= $("#test").attr("data-counter-number");
-        var time=$("#test").attr("data-counter-time");
-        var offset=$("#test").attr("data-counter-offset");
+            // console.log("視窗的上方:"+windowTop);
 
-        // console.log("數字"+number);
-        // console.log("時間"+time);
-        // console.log("位移"+offset);
+            // 取得參數:時間、數字與位移
+            var number = $(this).attr("data-counter-number");
+            var time = $(this).attr("data-counter-time");
+            var offset = $(this).attr("data-counter-offset");
 
-        // 判斷式
-        // 如果...發生，就...
-        // if(條件)(程式)
-        if (windowTop > top-offset) {
-            // console.log("開始執行動畫...");
+            // console.log("數字"+number);
+            // console.log("時間"+time);
+            // console.log("位移"+offset);
 
-            // 選取動畫({參數}，{選項})
-            $("#test").animate({
-                num : number            //數字
-            }, {
-                duration:parseInt(time),     //時間 - duration 需要數字 -parseInt()將字串轉為整數
-                step:(now)=> {      //步驟
-                    $("#test").text(Math.floor(now));     //文字(步驟)Math.floor去小數點
-                }
-            })
-        }
+            // 判斷式
+            // 如果...發生，就...
+            // if(條件)(程式)
+            if (windowTop > top - offset) {
+                // console.log("開始執行動畫...");
+
+                // 選取動畫({參數}，{選項})
+                $(this).animate({
+                    num: number            //數字
+                }, {
+                    duration: parseInt(time),     //時間 - duration 需要數字 -parseInt()將字串轉為整數
+                    step: (now) => {      //步驟
+                        $(this).text(Math.floor(now));     //文字(步驟)Math.floor去小數點
+                    }
+                })
+            }
+        });
     });
 });
